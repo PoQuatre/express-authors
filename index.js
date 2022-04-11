947106039040d0f628bbbef96670988a3ab37c43
PoQuatre <leflem.mateo2002@gmail.com>
2022-04-11 12:06:13 +0200

    feat: add exercise 3

--- index.js
+++ index.js
@@ -38,6 +38,17 @@ app.get("/authors/:id", (req, res) => {
   }
 });
 
+app.get("/authors/:id/books", (req, res) => {
+  const id = parseInt(req.params.id) - 1;
+  const author = authors[id];
+
+  if (author) {
+    res.send(author.books.join(", "));
+  } else {
+    res.status(404).send("There is no author with this id");
+  }
+});
+
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log("Server started on port:", port);
